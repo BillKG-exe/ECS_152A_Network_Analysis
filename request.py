c6597acdb6c80946fe62e6bfca9178358ba87c29
Billy Tahirou Ouattara <66051428+BillKG-exe@users.noreply.github.com>
2023-11-03 12:20:52 -0700

    Delete request.py

--- request.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-URL = "https://kartik-labeling-cvpr-0ed3099180c2.herokuapp.com/ecs152a_ass1"
-
-
-headers = {"Student-Id": "920603707"}
-response = requests.get(URL, headers=headers, verify=False)
